513121865feadea7ee3e950e2ba3ac0f0bb1d214
Dhara <dharababariya34@gmail.com>
2019-04-02 20:49:02 +0530

    Encrypt & Decyypt data

---
+++ app.js
@@ -0,0 +1,29 @@
+//Import module
+
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+
+
+
+//middleware
+
+app.use(bodyParser.json());
+
+//env config
+dotenv.config();
+
+
+
+//fire route
+
+app.use(require('./routes/save_controller'));
+app.use(require('./routes/get_contoller'));
+app.use(require('./routes/update_controller')) 
+
+
+//Create server
+app.listen(8080, () => {
+    console.log('listen port 8080');
+})
